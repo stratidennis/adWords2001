c2c3b46045ccadbd307f9d23fbdb95c0e765c801
stratidennis <46193140+stratidennis@users.noreply.github.com>
2020-10-19 12:11:35 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,13 +1,10 @@
-<?php
-require "functions.php";
-?>
-
 <!DOCTYPE html>
 <html lang='en'>
 
 <head>
     <meta charset='UTF-8'>
     <meta name='viewport' content='width=device-width, initial-scale=1.0'>
+    <link rel='shortcut icon' type='image/x-icon' href='favicon.ico'>
     <link href='https://fonts.googleapis.com/css?family=Roboto' rel='stylesheet'>
     <link rel='stylesheet' href='styles.css'>
     <title>AdWords</title>
